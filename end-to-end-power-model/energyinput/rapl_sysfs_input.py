0b9510ea08258f0f1d1d2096bf2d697e3b747567
David Freina <d.freina@student.vu.nl>
2024-06-20 12:00:02 +0200

    added rapl per process energy

--- end-to-end-power-model/energyinput/rapl_sysfs_input.py
+++ end-to-end-power-model/energyinput/rapl_sysfs_input.py
@@ -1,3 +1,5 @@
+import psutil
+
 from .generic_input import GenericInput
 
 
@@ -9,6 +11,7 @@ class RAPLSysFSInput(GenericInput):
         self.rapl_file = rapl_file
         self.previous_energy = self.read_energy()
         self.current_energy = 0
+        self.utilization = 0
 
     def __str__(self):
         return self.rapl_file.split('/').pop(-2)
@@ -19,10 +22,23 @@ class RAPLSysFSInput(GenericInput):
         return energy
 
     def get_energy(self) -> float:
+        per_core_usage = psutil.cpu_percent(percpu=True, interval=None)
+
+        for proc in psutil.process_iter(["name", "username"]):
+            try:
+                if "subscriber.py" in proc.cmdline():
+                    self.utilization = proc.cpu_percent(interval=None)
+                    proc_usage = proc.cpu_percent(interval=None) / sum(per_core_usage)
+                    break
+                else:
+                    self.utilization = per_core_usage
+                    proc_usage = 1
+            except psutil.ZombieProcess:
+                continue
         total_energy = self.read_energy()
         self.current_energy = self.handle_overflow(total_energy)
         self.previous_energy = total_energy
-        return self.current_energy / 1000000
+        return self.current_energy / 1000000 * proc_usage
 
     def handle_overflow(self, current_energy: int) -> int:
         if current_energy < self.previous_energy:
@@ -30,3 +46,6 @@ class RAPLSysFSInput(GenericInput):
             return overflow_correction
         else:
             return current_energy - self.previous_energy
+
+    def get_utilization(self) -> float:
+        return self.utilization
