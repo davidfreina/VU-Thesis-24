163460d10c20f598ba1fca4b0246f2314dea2bd7
David Freina <d.freina@student.vu.nl>
2024-06-20 23:06:05 +0200

    updated rapl per cpu measurement

--- end-to-end-power-model/energyinput/rapl_sysfs_input.py
+++ end-to-end-power-model/energyinput/rapl_sysfs_input.py
@@ -1,51 +1,55 @@
+from typing import List
+
 import psutil
+from typing import List
 
 from .generic_input import GenericInput
 
 
 class RAPLSysFSInput(GenericInput):
-    max_counter_value = 2 ** 64 - 1  # Assuming a 64-bit counter for RAPL
 
-    def __init__(self, rapl_file: str):
+    def __init__(self, rapl_files: List[str]):
         super().__init__()
-        self.rapl_file = rapl_file
+        self.rapl_files = rapl_files
         self.previous_energy = self.read_energy()
-        self.current_energy = 0
         self.utilization = 0
+        self.max_counter_value = 262143328850 * len(rapl_files)
 
     def __str__(self):
-        return self.rapl_file.split('/').pop(-2)
+        return self.rapl_files[0].split('/').pop(-2)
 
-    def read_energy(self) -> int:
-        with open(self.rapl_file, 'r') as file:
-            energy = int(file.read().strip())
-        return energy
+    def read_energy(self) -> List[int]:
+        energies = []
+        for rapl_file in self.rapl_files:
+            with open(rapl_file, 'r') as file:
+                energies.append(int(file.read().strip()))
+        return energies
 
     def get_energy(self) -> float:
         per_core_usage = psutil.cpu_percent(percpu=True, interval=None)
-
+        proc_usage = 0
         for proc in psutil.process_iter(["name", "username"]):
             try:
                 if "subscriber.py" in proc.cmdline():
-                    self.utilization = proc.cpu_percent(interval=None)
-                    proc_usage = proc.cpu_percent(interval=None) / sum(per_core_usage)
-                    break
-                else:
-                    self.utilization = per_core_usage
-                    proc_usage = 1
+                    proc_usage += proc.cpu_percent(interval=None)
+                    self.utilization = proc_usage
             except psutil.ZombieProcess:
                 continue
-        total_energy = self.read_energy()
-        self.current_energy = self.handle_overflow(total_energy)
-        self.previous_energy = total_energy
-        return self.current_energy / 1000000 * proc_usage
+            except psutil.NoSuchProcess:
+                continue
+        energies = self.read_energy()
+        current_energy = 0
+        for idx, energy in enumerate(energies):
+            current_energy += self.handle_overflow(energy, idx)
+        self.previous_energy = energies
+        return current_energy / 1000000 * (proc_usage / sum(per_core_usage))
 
-    def handle_overflow(self, current_energy: int) -> int:
-        if current_energy < self.previous_energy:
-            overflow_correction = self.max_counter_value - self.previous_energy + current_energy + 1
+    def handle_overflow(self, current_energy: int, index: int) -> int:
+        if current_energy < self.previous_energy[index]:
+            overflow_correction = self.max_counter_value - self.previous_energy[index] + current_energy + 1
             return overflow_correction
         else:
-            return current_energy - self.previous_energy
+            return current_energy - self.previous_energy[index]
 
     def get_utilization(self) -> float:
         return self.utilization
